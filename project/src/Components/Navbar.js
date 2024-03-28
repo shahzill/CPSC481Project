5bff61fa2ed1f0bf0fc2ab7208c57f946c1793ee
Shahzill Naveed <101993148+shahzill@users.noreply.github.com>
2024-03-28 16:54:24 -0600

    Search feature

--- project/src/Components/Navbar.js
+++ project/src/Components/Navbar.js
@@ -1,45 +1,55 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useRef } from "react";
-import { HashLink as Link} from 'react-router-hash-link'
-import "../Style/Navbar.css"
+import { HashLink as Link } from "react-router-hash-link";
+import "../Style/Navbar.css";
 
 function Navbar() {
-	const navRef = useRef();
-	const [navColor,setNavColor] = useState(false);
+  const navRef = useRef();
+  const [navColor, setNavColor] = useState(false);
 
-	const showNavbar = () => {
-		navRef.current.classList.toggle(
-			"responsive_nav"
-		);
-	};
+  const changeBG = () => {
+    if (window.scrollY >= 10) {
+      setNavColor(true);
+    } else {
+      setNavColor(false);
+    }
+  };
 
-	const changeBG = () => {
-		if(window.scrollY >= 10){
-			setNavColor(true)
-		}
-		else{
-			setNavColor(false)
-		}
-	}
+  window.addEventListener("scroll", changeBG);
 
-	window.addEventListener('scroll', changeBG)
+  return (
+    <header className="header">
+      <Link
+        className="link dancingScript"
+        smooth
+        to="/#"
+        style={{ marginLeft: "10px" }}
+      >
+        Culinary Canvas
+      </Link>
 
-	return (
-		<header className="header">
-			<Link className="link dancingScript" smooth to="/#" style={{ marginLeft: '10px' }}>Culinary Canvas</Link>
-
-			<nav ref={navRef}>
-                <div className="links">
-                    <Link className="link" smooth to="/" >Home</Link>
-                    <Link className="link" smooth to="/Menu" >Menu</Link>
-                    <Link className="link" smooth to="/Order" >Order</Link>
-                    <Link className="link" smooth to="/Payment" >Payment</Link>
-                    <Link className="link" smooth to="" >About us</Link>
-                </div>
-			</nav>
-			<button className="CallForAssistance" >Call for assistance</button>
-		</header>
-	);
+      <nav ref={navRef}>
+        <div className="links">
+          <Link className="link" smooth to="/">
+            Home
+          </Link>
+          <Link className="link" smooth to="/Menu">
+            Menu
+          </Link>
+          <Link className="link" smooth to="/Order">
+            Order
+          </Link>
+          <Link className="link" smooth to="/Payment">
+            Payment
+          </Link>
+          <Link className="link" smooth to="">
+            About us
+          </Link>
+        </div>
+      </nav>
+      <button className="CallForAssistance">Call for assistance</button>
+    </header>
+  );
 }
 
 export default Navbar;
--- project/src/Pages/Menu.js
+++ project/src/Pages/Menu.js
@@ -1,31 +1,131 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../Components/Navbar";
 import "../Style/Navbar.css";
 import "../Style/Menu.css";
 import { foodItems } from "../Data/FoodItems";
 import steakFries from "../Images/SteakFries.jpg";
 import { Link } from "react-router-dom";
+import { HashLink as ScrollLink } from "react-router-hash-link";
+import { animateScroll as scroll } from "react-scroll";
 
 function MenuPage() {
   // Find the specific food item
-  const specificFoodItem =
-    foodItems.find((foodItem) => foodItem.id === 2) || null;
-  const Soups = foodItems.filter((foodItem) => foodItem.Course === "Soups");
-  const Appetizers = foodItems.filter(
-    (foodItem) => foodItem.Course === "Appetizers"
-  );
-  const Salad = foodItems.filter((foodItem) => foodItem.Course === "Salad");
-  const Fish = foodItems.filter((foodItem) => foodItem.Course === "Fish");
-  const MainCourse = foodItems.filter(
-    (foodItem) => foodItem.Course === "Main course"
-  );
-  const PalateCleasner = foodItems.filter(
-    (foodItem) => foodItem.Course === "Palate cleanser"
-  );
-  const Dessert = foodItems.filter(
-    (foodItem) => foodItem.Course === "Desserts"
-  );
-  const Drinks = foodItems.filter((foodItem) => foodItem.Course === "Drinks");
+  const [searchValue, setSearchValue] = useState("");
+  const [Soups, setSoups] = useState([]);
+  const [Appetizers, setAppetizers] = useState([]);
+  const [Salad, setSalad] = useState([]);
+  const [Fish, setFish] = useState([]);
+  const [MainCourse, setMainCourse] = useState([]);
+  const [PalateCleasner, setPalateCleasner] = useState([]);
+  const [Dessert, setDessert] = useState([]);
+  const [Drinks, setDrinks] = useState([]);
+
+  useEffect(() => {
+    updateFoodData();
+  }, []);
+
+  function updateFoodData() {
+    console.log("calling usestate");
+    if (searchValue != null && searchValue.length > 1) {
+      console.log("calling usestate yesyyys");
+      setSoups(
+        foodItems.filter(
+          (foodItem) =>
+            foodItem.Course === "Soups" &&
+            foodItem.Name.toLowerCase().includes(searchValue.toLowerCase())
+        )
+      );
+
+      setAppetizers(
+        foodItems.filter(
+          (foodItem) =>
+            foodItem.Course === "Appetizers" &&
+            foodItem.Name.toLowerCase().includes(searchValue.toLowerCase())
+        )
+      );
+
+      setSalad(
+        foodItems.filter(
+          (foodItem) =>
+            foodItem.Course === "Salad" &&
+            foodItem.Name.toLowerCase().includes(searchValue.toLowerCase())
+        )
+      );
+
+      setFish(
+        foodItems.filter(
+          (foodItem) =>
+            foodItem.Course === "Fish" &&
+            foodItem.Name.toLowerCase().includes(searchValue.toLowerCase())
+        )
+      );
+
+      setMainCourse(
+        foodItems.filter(
+          (foodItem) =>
+            foodItem.Course === "Main course" &&
+            foodItem.Name.toLowerCase().includes(searchValue.toLowerCase())
+        )
+      );
+
+      setPalateCleasner(
+        foodItems.filter(
+          (foodItem) =>
+            foodItem.Course === "Palate cleanser" &&
+            foodItem.Name.toLowerCase().includes(searchValue.toLowerCase())
+        )
+      );
+
+      setDessert(
+        foodItems.filter(
+          (foodItem) =>
+            foodItem.Course === "Desserts" &&
+            foodItem.Name.toLowerCase().includes(searchValue.toLowerCase())
+        )
+      );
+
+      setDrinks(
+        foodItems.filter(
+          (foodItem) =>
+            foodItem.Course === "Drinks" &&
+            foodItem.Name.toLowerCase().includes(searchValue.toLowerCase())
+        )
+      );
+    } else {
+      setSoups(foodItems.filter((foodItem) => foodItem.Course === "Soups"));
+
+      setAppetizers(
+        foodItems.filter((foodItem) => foodItem.Course === "Appetizers")
+      );
+
+      setSalad(foodItems.filter((foodItem) => foodItem.Course === "Salad"));
+
+      setFish(foodItems.filter((foodItem) => foodItem.Course === "Fish"));
+
+      setMainCourse(
+        foodItems.filter((foodItem) => foodItem.Course === "Main course")
+      );
+
+      setPalateCleasner(
+        foodItems.filter((foodItem) => foodItem.Course === "Palate cleanser")
+      );
+
+      setDessert(
+        foodItems.filter((foodItem) => foodItem.Course === "Desserts")
+      );
+
+      setDrinks(foodItems.filter((foodItem) => foodItem.Course === "Drinks"));
+    }
+  }
+  const handleSearchChange = (event) => {
+    // Update the width dynamically based on the input value
+    const { value } = event.target;
+
+    // Set the input value to the entered word
+    setSearchValue(value);
+    console.log(searchValue);
+    updateFoodData();
+  };
 
   return (
     <>
@@ -38,21 +138,76 @@ function MenuPage() {
         <div className="contentMenuPage">
           <div className="search-bar">
             Search menu:&nbsp;&nbsp;
-            <input type="text" placeholder="Search..." />
+            <input
+              type="text"
+              style={{ width: "200px", height: "40px", fontSize: "20px" }}
+              className="search-menu-bar"
+              value={searchValue}
+              placeholder="Search..."
+              onChange={handleSearchChange}
+            />
           </div>
           <div className="menu">
             <div className="courses">
               <div className="courses-header">Courses</div>
               <div className="courses-list">
                 <ul>
-                  <li>Soups</li>
-                  <li>Appetizers</li>
-                  <li>Salad</li>
-                  <li>Fish</li>
-                  <li>Main course</li>
-                  <li>Palate cleanser</li>
-                  <li>Dessert</li>
-                  <li>Drinks</li>
+                  <ScrollLink
+                    className="font-size-link-course"
+                    smooth
+                    to="#Soups"
+                  >
+                    <li>Soups</li>
+                  </ScrollLink>
+                  <ScrollLink
+                    className="font-size-link-course"
+                    smooth
+                    to="#Appetizers"
+                  >
+                    <li>Appetizers</li>
+                  </ScrollLink>
+                  <ScrollLink
+                    className="font-size-link-course"
+                    smooth
+                    to="#Salad"
+                  >
+                    <li>Salads</li>
+                  </ScrollLink>
+                  <ScrollLink
+                    className="font-size-link-course"
+                    smooth
+                    to="#fish"
+                  >
+                    <li>Fish</li>
+                  </ScrollLink>
+                  <ScrollLink
+                    className="font-size-link-course"
+                    smooth
+                    to="#MainCourse"
+                  >
+                    <li>Main courses</li>
+                  </ScrollLink>
+                  <ScrollLink
+                    className="font-size-link-course"
+                    smooth
+                    to="#PalateCleanser"
+                  >
+                    <li>Palate cleansers</li>
+                  </ScrollLink>
+                  <ScrollLink
+                    className="font-size-link-course"
+                    smooth
+                    to="#Desserts"
+                  >
+                    <li>Desserts</li>
+                  </ScrollLink>
+                  <ScrollLink
+                    className="font-size-link-course"
+                    smooth
+                    to="#Drinks"
+                  >
+                    <li>Drinks</li>
+                  </ScrollLink>
                 </ul>
               </div>
               <div className="key-header">Key</div>
@@ -65,7 +220,9 @@ function MenuPage() {
             </div>
             <div className="menu-items">
               <div className="food-items-courses">
-                <div className="food-items-courses-header">Soups</div>
+                <div id="Soups" className="food-items-courses-header">
+                  Soups
+                </div>
                 <div className="food-items-courses-items">
                   {Soups.map((item, index) => (
                     <Link
@@ -89,7 +246,9 @@ function MenuPage() {
                 </div>
               </div>
               <div className="food-items-courses">
-                <div className="food-items-courses-header">Appetizers</div>
+                <div id="Appetizers" className="food-items-courses-header">
+                  Appetizers
+                </div>
                 <div className="food-items-courses-items">
                   {Appetizers.map((item, index) => (
                     <Link
@@ -110,7 +269,9 @@ function MenuPage() {
                 </div>
               </div>
               <div className="food-items-courses">
-                <div className="food-items-courses-header">Salad</div>
+                <div id="Salad" className="food-items-courses-header">
+                  Salad
+                </div>
                 <div className="food-items-courses-items">
                   {Salad.map((item, index) => (
                     <Link
@@ -131,7 +292,9 @@ function MenuPage() {
                 </div>
               </div>
               <div className="food-items-courses">
-                <div className="food-items-courses-header">Fish</div>
+                <div id="fish" className="food-items-courses-header">
+                  Fish
+                </div>
                 <div className="food-items-courses-items">
                   {Fish.map((item, index) => (
                     <Link
@@ -152,7 +315,9 @@ function MenuPage() {
                 </div>
               </div>
               <div className="food-items-courses">
-                <div className="food-items-courses-header">Main Course</div>
+                <div id="MainCourse" className="food-items-courses-header">
+                  Main Course
+                </div>
                 <div className="food-items-courses-items">
                   {MainCourse.map((item, index) => (
                     <Link
@@ -173,7 +338,9 @@ function MenuPage() {
                 </div>
               </div>
               <div className="food-items-courses">
-                <div className="food-items-courses-header">Palate Cleasner</div>
+                <div id="PalateCleanser" className="food-items-courses-header">
+                  Palate Cleanser
+                </div>
                 <div className="food-items-courses-items">
                   {PalateCleasner.map((item, index) => (
                     <Link
@@ -194,7 +361,9 @@ function MenuPage() {
                 </div>
               </div>
               <div className="food-items-courses">
-                <div className="food-items-courses-header">Desserts</div>
+                <div id="Desserts" className="food-items-courses-header">
+                  Desserts
+                </div>
                 <div className="food-items-courses-items">
                   {Dessert.map((item, index) => (
                     <Link
@@ -215,7 +384,9 @@ function MenuPage() {
                 </div>
               </div>
               <div className="food-items-courses">
-                <div className="food-items-courses-header">Drinks</div>
+                <div id="Drinks" className="food-items-courses-header">
+                  Drinks
+                </div>
                 <div className="food-items-courses-items">
                   {Drinks.map((item, index) => (
                     <Link
